c919505b19cef7202b97bb9a1fc2935c1e7eff2a
Farbod Nezafati <farbodneza@gmail.com>
2024-08-30 14:56:22 +0330

    Array

---
+++ js-basics/Array.js
@@ -0,0 +1,4 @@
+let selectedColor = ['Red', 'blue'];
+selectedColor[2] = '2';
+console.log(selectedColor.length);
+console.log(selectedColor[0]);
--- js-basics/index.js
+++ js-basics/index.js
@@ -6,3 +6,26 @@ console.log(name);
 //Cannot start with a number (1name)
 //Cannot contain a space or hyphen (-)
 //Are case-sensitive
+
+
+//Constant
+const interestRate = 0.3;
+console.log(interestRate);
+
+//instead using many vars we just use obgect
+//let lastName = 'Nezafati';
+//let age = 20;
+let person = {
+    name: 'Farbpd',
+    lastName: 'Nezafati',
+    age: 20
+};
+
+// Dot Notation
+person.name = 'Rose';
+
+// Bracket Notation
+person['age'] = 16;
+
+console.log(person);
+console.log(person.name);
